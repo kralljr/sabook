a42861ea91399aaf33e812c472a604da712e9ba6
kralljr <jenna.krall@gmail.com>
2016-03-24 12:36:52 -0400

    Remove fireworks days, bad days

--- nycdata.R
+++ nycdata.R
@@ -36,4 +36,24 @@ length(dates)
 nycdat <- nycdat[keeps, ]
 
 
+
+#########
+# Bad dates
+# Remove bad dates
+dates <- c("2006-01-05", "2006-01-20") %>% as.Date()
+# Remove days around 4th of July, New Years
+month <- substr(nycdat[, 1], 6, 7) %>% as.numeric()
+day <- substr(nycdat[, 1], 9, 10) %>% as.numeric()
+keeps <- !(nycdat$Date %in% dates | (month == 1 & day == 1) |
+  (month == 12 & day == 31) | (month == 7 & day == 4) |
+  (month == 7 & day == 5) | (month == 7 & day == 3) |
+  (month == 11 & day == 11))
+nycdat <- nycdat[keeps, ]
+
+
+
+
+
+
+
 write.csv(nycdat, file = "nycdat.csv", row.names = F)

24146010eb049d4000a074b27dbd1390e09662e6
kralljr <jenna.krall@gmail.com>
2016-03-24 11:57:15 -0400

    Limit data to continuously monitored time period 2003-2006

--- nycdata.R
+++ nycdata.R
@@ -1,9 +1,7 @@
 
 # Load libraries
-library(handles)
 library(share)
 library(dplyr)
-library(ggplot2)
 library(tidyr)
 
 
@@ -16,5 +14,26 @@ monNY <- filter(monitorsNE, monitor == "36081.0124")
 
 # verified from CSN website
 nycdat <- consConc[["36081.0124"]]
+
+
+
+
+###################
+# Find time period
+
+# 2003 - 2006
+years <- substr(nycdat[, 1], 1, 4) %>% as.numeric()
+keeps <- years <= 2006 & years >= 2003
+
+# Check summary
+dates <- nycdat[keeps, 1]
+summary(dates)
+diff1 <- diff(dates)
+summary(as.numeric(diff1))
+length(dates)
+
+
+nycdat <- nycdat[keeps, ]
+
 
 write.csv(nycdat, file = "nycdat.csv", row.names = F)

96dcdcdcdaa4c1f51f8412d351facedaace97331
kralljr <jenna.krall@gmail.com>
2016-03-24 12:56:21 -0400

    Add concentration and prof plots

--- pca-apca.R
+++ pca-apca.R
@@ -41,6 +41,9 @@ nf <- numfact(nycdat[, -c(1, 2)], seed1 = 895833)
 apca1 <- apca(nycdat[, -2], nycdat[, 2], nsources = nf)
 ncons <- ncol(nycdat) - 2
 
+
+####
+# PROFILES
 profs <- apca1$vmax$loadings[1 : ncons, ]
 # Make more positive
 profs <- sweep(profs, 2, sign(colSums(profs)), "*")
@@ -50,13 +53,27 @@ profs <- data.frame(rownames(profs), profs)
 colnames(profs)[1] <- "cons"
 profs <- gather(profs, source, value, -cons)
 
+###
+# CONCENTRATIONS
+conc <- data.frame(nycdat[, 1], apca1$conc)
+colnames(conc)[1] <- "Date"
+# fix
+conc <- gather(conc, source, value, -Date)
 
 
-
-ggplot(profs, aes(x = cons, y = value, fill = value)) + 
+###
+# Plots of APCA results
+profsplot <- ggplot(profs, aes(x = cons, y = value, fill = value)) + 
     geom_bar(stat="identity") +
     xlab("") + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust= 0.4)) + 
     facet_wrap(~ source, ncol =3)
+pdf("apca-profplot.pdf")
+profsplot
+dev.off()
 
 
-
+concplot <- ggplot(conc, aes(x = Date, y = value)) + geom_line() + 
+ geom_point() + facet_wrap(~ source, ncol = 3) 
+pdf("apca-concplot.pdf")
+concplot
+dev.off()
